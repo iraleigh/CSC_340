dbf430c33523b349cc047ac7db9e66f21c450337
iraleigh <iain.raleigh.dev@gmail.com>
2015-02-25 13:16:56 -0800

    fixed a bug that kept it from multiplying matrices of diffent dimensions

--- CSC_340_HW2/Matrix_Multiplication_Project/Matrix_Multiplication.cpp
+++ CSC_340_HW2/Matrix_Multiplication_Project/Matrix_Multiplication.cpp
@@ -3,28 +3,28 @@
 
 using namespace std;
 
-void multiply_matricies(vector< vector<int> >&, vector< vector<int> >&);
+vector< vector <int> > multiply_matricies(vector< vector<int> >&, vector< vector<int> >&);
 
 int main(){
-	vector< vector<int> > A,B;
+	vector< vector<int> > A,B,C;
 	int A_rows, A_columns;
 	int B_rows, B_columns;
 	cout << "How many rows are in matrix A? ";
 	cin >> A_rows;
 	cout << "How many columns are in matrix A? ";
 	cin >> A_columns;
-	A.resize(A_columns);
+	A.resize(A_rows);
 	for (int a = 0; a < A.size(); a++){
-		A[a].resize(A_rows);
+		A[a].resize(A_columns);
 	}
 	cout << "How many rows are in matrix B? ";
 	cin >> B_rows;
 	cout << "How many columns are in matrix B? ";
 	cin >> B_columns;
 
-	B.resize(B_columns);
+	B.resize(B_rows);
 	for (int b = 0; b < B.size(); b++){
-		B[b].resize(B_rows);
+		B[b].resize(B_columns);
 	}
 
 	for (int ai = 0; ai < A_rows; ai++) {
@@ -44,28 +44,30 @@ int main(){
 
 	cout << "\n\nCalculating...\n\n";
 
-	cout << "The product of A and B is: \n";
-	multiply_matricies(A,B);
-	return 0;
-}
 
-void multiply_matricies(vector< vector<int> >& A, vector< vector<int> >& B){
-	vector< vector<int> > C;
-	C.resize(B.size());
-	for (int c = 0; c < C.size(); c++){
-		C[c].resize(A.size());
-	}
-	for (int i = 0; i < C.size();i++){
-		for(int j = 0; j < C[0].size(); j++){
-			 for(int k = 0; k < C[0].size(); k++){
-				C[i][j] += A[i][k] * B[k][j];
-			}
-		}
-	}
+	cout << "The product of A and B is: \n";
+	C = multiply_matricies(A,B);
 	for (int i = 0; i < C.size();i++){
 		for(int j = 0; j < C[0].size(); j++){
 			cout << C[i][j] << " ";
 		}
 		cout << endl;
 	}
+	return 0;
+}
+
+vector< vector<int > > multiply_matricies(vector< vector<int> >& A, vector< vector<int> >& B){
+	vector< vector<int> > C;
+	C.resize(B[0].size());
+	for (int c = 0; c < C.size(); c++){
+		C[c].resize(A.size());
+	}
+	for (int i = 0; i < C.size();i++){
+		for(int j = 0; j < C[0].size(); j++){
+			 for(int k = 0; k < A[0].size(); k++){
+				C[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+	return C;
 }
